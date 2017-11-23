1ab8f393ac3ca776b4818db53f637dbf46c9cbe3
Anderson Singh <andersonsingh@outlook.com>
2017-11-23 19:31:53 -0400

    removed sudo

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get install -y python-software-properties debconf-utils
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-apt-get install -y oracle-java8-installer
+# apt-get install -y python-software-properties debconf-utils
+# add-apt-repository -y ppa:webupd8team/java
+# apt-get update
+# echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+# apt-get install -y oracle-java8-installer
 
-wget https://www.dropbox.com/s/zpkdkyf0c9pkupu/Azure%20ServiceBus%20Application.jar
-java -jar "Azure ServiceBus Application.jar"
+# wget https://www.dropbox.com/s/zpkdkyf0c9pkupu/Azure%20ServiceBus%20Application.jar
+# java -jar "Azure ServiceBus Application.jar"
 
 
