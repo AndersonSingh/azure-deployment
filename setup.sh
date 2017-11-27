d2a42450b83bec49a680fa25c866876d24e151a0
Anderson Singh <andersonsingh@outlook.com>
2017-11-27 16:26:46 -0400

    modified setup script with new info

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 apt-get install -y oracle-java8-installer
 
-wget https://www.dropbox.com/s/zpkdkyf0c9pkupu/Azure%20ServiceBus%20Application.jar
-java -jar "Azure ServiceBus Application.jar" > log.txt &
+wget https://www.dropbox.com/s/op3jttexge4ig2s/Azure%20ServiceBus%20Application.jar
+java -jar "Azure ServiceBus Application.jar" DefaultEndpointsProtocol=https;AccountName=andersonshiva;AccountKey=ZpZVD8FhD7sg9/BSM1pWk9z2g8JhyJ0VipEVhXc8vX7GxeJvqkNVFeestim8tdAU03QEUvmfnVTmw55GvBnbOQ==;EndpointSuffix=core.windows.net Endpoint=sb://andersonshiva.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rz+B4meO8sMBXsM+QQXjAeoFdNTgwcNamyXIdLnAQkk= locustrequests > log.txt &
