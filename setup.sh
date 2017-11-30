6f57901a976260c261b951fdc399636e512d63ea
Anderson Singh <andersonsingh@outlook.com>
2017-11-29 22:13:39 -0400

    updated dropbox link

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 apt-get install -y oracle-java8-installer
 
-wget https://www.dropbox.com/s/jvv1zzxcaizwrdk/servicebus.jar
+wget https://www.dropbox.com/s/v57cbj78w1fjhf7/servicebus.jar
 java -jar servicebus.jar &
